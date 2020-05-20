f1f9d4be88502bae5773ccf4b175655072c76697
Hector Ventura <hectorvent@gmail.com>
2020-05-20 01:07:42 -0500

    Fixed centos7 build image and added Ubuntu focal as builder image

--- buildenv/prepare.sh
+++ buildenv/prepare.sh
@@ -2,8 +2,7 @@
 
 DOCKER_IMAGE=hectorvent/tg2sip-builder
 
-for DOCKER_TAG in bionic buster
-# for DOCKER_TAG in bionic buster centos6 centos7
+for DOCKER_TAG in bionic focal buster centos7
 do
     docker build . -f Dockerfile."$DOCKER_TAG" -t "$DOCKER_IMAGE:$DOCKER_TAG"
     docker push "$DOCKER_IMAGE:$DOCKER_TAG"
